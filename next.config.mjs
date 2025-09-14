efb62ef5ad363f54fab41b7476ae777fef8ce5ab
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2025-09-14 18:43:41 +0500

    udpated next ocnfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // forces `next build` to also run `next export`
+  images: {
+    unoptimized: true, // disables Image Optimization (not supported on GitHub Pages)
+  },
+};
 
 export default nextConfig;
